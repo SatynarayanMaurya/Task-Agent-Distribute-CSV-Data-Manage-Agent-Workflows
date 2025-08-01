04d3c6caee07bb9590968db9b7012578c0e865d8
Satynarayan Maurya <123947617+SatynarayanMaurya@users.noreply.github.com>
2025-08-01 11:45:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(express.json());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://task-agent-distribute-csv-data-mana.vercel.app",
     credentials:true
 }))
 
